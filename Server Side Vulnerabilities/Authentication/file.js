875b738d4a8e6a1e9e30ffae9b489ce73eb4d4c8
Majed <158180710+0xMajedf@users.noreply.github.com>
2025-03-29 15:29:22 +0300

    creating a js file

---
+++ Server Side Vulnerabilities/Authentication/file.js
@@ -0,0 +1,33 @@
+// Variable and values Assignment First one
+
+/*
+let country = "Saudi arabia";
+let continent = "Asia";
+let population = "37.000.000";
+
+console.log(country);
+console.log(continent);
+console.log(population);
+*/
+
+
+// Data Types Assigment Second one 
+
+/*
+let isIsland = false;
+let Language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof Language);
+*/
+
+
+let age = 30;
+age = 31;
+
+const birthYear = 2002;
+birthYear = 1990;
+
+console.log(birthYear);
